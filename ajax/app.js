65a13403317b0ede3852581d1b887570bc0f6286
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-26 10:58:42 -0400

    AJAX: Append characters to the page

--- ajax/app.js
+++ ajax/app.js
@@ -7,7 +7,21 @@ $(document).on("ready", function () {
     $.ajax({
       url: "https://ironhack-characters.herokuapp.com/characters",
 
-      success: function () { console.log("It worked!"); },
+      success: function (theData) {
+        console.log("It worked!");
+        console.log(theData);
+        console.log(theData.length);
+
+        theData.forEach(function (oneCharacter) {
+          var html = `
+            <li>
+              <p>Name: ${oneCharacter.name}</p>
+              <p>Occupation: ${oneCharacter.occupation}</p>
+            </li>`;
+
+          $(".js-character-list").append(html);
+        });
+      },
 
       error: function () { console.log("It failed. :("); }
     });
