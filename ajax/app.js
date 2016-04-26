6fb0f451bb8f943d08d1226017ef4fee4d8f9efa
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-26 11:07:21 -0400

    AJAX: Factor part of the success callback

--- ajax/app.js
+++ ajax/app.js
@@ -5,27 +5,34 @@ $(document).on("ready", function () {
   $(".js-character-ajax").on("click", function () {
 
     $.ajax({
-      url: "https://ironhack-characters.herokuapp.com/characters",
+      url: "https://ironhack-characters.herokuapp.com/skjdhfksjhkfjhjfhsk",
 
       success: function (theData) {
         console.log("It worked!");
         console.log(theData);
-        console.log(theData.length);
 
-        theData.forEach(function (oneCharacter) {
-          var html = `
-            <li>
-              <p>Name: ${oneCharacter.name}</p>
-              <p>Occupation: ${oneCharacter.occupation}</p>
-            </li>`;
-
-          $(".js-character-list").append(html);
-        });
+        displayCharacters(theData);
       },
 
-      error: function () { console.log("It failed. :("); }
+      error: function (theError) {
+        console.log("It failed. :( ");
+        console.log(theError.responseJSON);
+      }
     });
 
   });
 
 });
+
+
+function displayCharacters (characters) {
+  characters.forEach(function (oneCharacter) {
+    var html = `
+      <li>
+        <p>Name: ${oneCharacter.name}</p>
+        <p>Occupation: ${oneCharacter.occupation}</p>
+      </li>`;
+
+    $(".js-character-list").append(html);
+  });
+}
