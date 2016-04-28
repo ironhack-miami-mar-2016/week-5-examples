e61964cd4f285b638950e288de35f3d32fbabf32
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-28 09:36:28 -0400

    Spotify Artists: Add album search

--- spotify_artists/main.js
+++ spotify_artists/main.js
@@ -1,5 +1,6 @@
 $(document).on("ready", function(){
-	$(".js-artist-submit").on("click", getSpotifyArtists)
+	$(".js-artist-submit").on("click", getSpotifyArtists);
+	$(".artist-list").on("click", ".js-artist-albums", getSpotifyAlbums);
 })
 
 function getSpotifyArtists(){
@@ -10,11 +11,11 @@ function getSpotifyArtists(){
 
 	$.ajax({
 		url: "https://api.spotify.com/v1/search?type=artist&query=" + searchTerm,
-		success: onArtistFindSuccess, 
+		success: onArtistFindSuccess,
 		error: onArtistFindError
-	})
+	});
 	// This clears the input box's term
-	$('.js-artist-input').val("")
+	$('.js-artist-input').val("");
 }
 
 function onArtistFindSuccess(artistResponse){
@@ -29,21 +30,63 @@ function createArtistHtml(artist){
 	var image;
 
 	if (artist.images.length > 0){
-		image = artist.images[0].url
+		image = artist.images[0].url;
 	} else {
-		image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCk2bayZHUJsWeglTeTOvjcX3PvSpnkqU3T-6YmCE6bT1nFQ56Bw"
+		image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCk2bayZHUJsWeglTeTOvjcX3PvSpnkqU3T-6YmCE6bT1nFQ56Bw";
 	}
 
 	var html = `
-	<li class="artist-item">
-		<h4>${artist.name}</h4>
-		<img class="artist-image" src="${image}">
-	</li>
-	`;
+		<li class="artist-item">
+			<h4>${artist.name}</h4>
+			<img class="artist-image" src="${image}">
+
+			<button class="albums-btn js-artist-albums" data-blah="${artist.id}">
+				Show albums for ${artist.name}
+			</button>
+		</li>`;
 
 	$(".artist-list").append(html);
 }
 
-function onArtistFindError(err){
-	console.log("You dun goofed", err);
+function onArtistFindError(error){
+	console.log("You dun goofed",  error.responseJSON);
+}
+
+
+function getSpotifyAlbums (event) {
+	var btn = event.currentTarget;
+	var artistId = $(btn).data("blah");
+
+	$.ajax({
+		url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+		success: onAlbumsSuccess,
+		error: onAlbumsError
+	});
+}
+
+function onAlbumsSuccess (data) {
+	$(".js-albums-list").empty();
+
+	data.items.forEach(function (album) {
+		var image;
+
+		if (album.images.length > 0){
+			image = album.images[0].url;
+		} else {
+			image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCk2bayZHUJsWeglTeTOvjcX3PvSpnkqU3T-6YmCE6bT1nFQ56Bw";
+		}
+
+		var html = `
+			<li>
+				<h4>${album.name}</h4>
+				<img class="artist-image" src="${image}">
+			</li>`;
+
+		$(".js-albums-list").append(html);
+	});
+
+}
+
+function onAlbumsError (error) {
+	console.log("You dun goofed", error.responseJSON);
 }
