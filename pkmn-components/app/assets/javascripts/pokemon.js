650c24a4418c28ca7b6053e637b8338a2063f288
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-04-29 15:45:04 -0400

    Pokemon: Add Pokemon component

--- pkmn-components/app/assets/javascripts/pokemon.js
+++ pkmn-components/app/assets/javascripts/pokemon.js
@@ -1,2 +1,52 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+PokemonApp.Pokemon = function (pokemonUri) {
+  this.id = PokemonApp.Pokemon.idFromUri(pokemonUri);
+};
+
+PokemonApp.Pokemon.prototype.render = function () {
+
+  console.log("Rendering pokemon: #" + this.id);
+
+  $.ajax({
+    url: `/api/pokemon/${this.id}`,
+
+    success: function (response) {
+      console.log("Pokemon info:");
+      console.log(response);
+
+      $(".js-pkmn-name").text(response.name);
+      $(".js-pkmn-number").text("#" + response.pkdx_id);
+      $(".js-pkmn-height").text(response.height);
+      $(".js-pkmn-weight").text(response.weight);
+
+      $(".js-pkmn-modal").modal("show");
+    },
+
+    error: function (error) {
+      console.log("Oh no! Error. :( ");
+      console.log(error.responseJSON);
+    }
+  });
+
+};
+
+PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
+  var uriSegments = pokemonUri.split("/");
+  var secondLast = uriSegments.length - 2;
+  return uriSegments[secondLast];
+};
+
+
+// =============================================================================
+
+
+$(document).on("ready", function () {
+
+  $(".js-show-pokemon").on("click", function (event) {
+    var $button = $(event.currentTarget);
+    var pokemonUri = $button.data("pokemon-uri");
+
+    var pokemon = new PokemonApp.Pokemon(pokemonUri);
+    pokemon.render();
+  });
+
+});
