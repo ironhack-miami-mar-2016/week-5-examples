4ba0f8efba48dc8159b0750d862a1da093435eb7
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-05-02 09:35:14 -0400

    Spotify Player: Play and pause audio

--- spotify-skeleton/app.js
+++ spotify-skeleton/app.js
@@ -3,6 +3,7 @@
 $(document).on("ready", function () {
 
   $(".js-search-btn").on("click", onSearch);
+  $(".js-player-btn").on("click", playOrPause);
 
 });
 
@@ -34,4 +35,19 @@ function updatePlayer (searchResults) {
   $(".js-player-title").text(theTrack.name);
   $(".js-player-artist").text(theTrack.artists[0].name);
   $(".js-player-img").prop("src", theTrack.album.images[0].url);
+  $(".js-player-audio").prop("src", theTrack.preview_url);
+  $(".js-player-btn").removeClass("disabled");
+}
+
+
+function playOrPause () {
+  $(".js-player-btn").toggleClass("playing");
+
+  if ($(".js-player-btn").hasClass("playing")) {
+    $(".js-player-audio").trigger("play");
+  }
+
+  else {
+    $(".js-player-audio").trigger("pause");
+  }
 }
